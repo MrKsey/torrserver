057c3cad56d9e7e1639c6984b68dba2d17f7ed25
ksey <ksey3000@gmail.com>
2021-03-05 11:54:16 +0300

    Update update_TS.sh

--- update_TS.sh
+++ update_TS.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-[ -d /TS/updates ] && rmdir --ignore-fail-on-non-empty /TS/updates
+echo " "
+echo "Start checking for updates ..."
+[ -d "/TS/updates" ] && rm -r /TS/updates
 mkdir -p /TS/updates
 wget --output-document=/TS/updates/TorrServer --tries=3 $(curl -s $TS_URL/$TS_RELEASE | grep browser_download_url | egrep -o 'http.+\w+' | grep -i "$(dpkg --print-architecture)" | grep -m 1 -i $LINUX_NAME)
 chmod a+x /TS/updates/TorrServer
@@ -10,11 +12,17 @@ if [ $? -eq 0 -a ! -z "$updated_ver" ]; then
     if [ "$updated_ver" != "$current_ver" ]; then
         echo "Updating to $updated_ver ..."
         pkill TorrServer
-        [ -d /TS/backup ] && rmdir --ignore-fail-on-non-empty /TS/backup
+        [ -d "/TS/backup" ] && rm -r /TS/backup
         mkdir -p /TS/backup
         cp -f /TS/TorrServer /TS/backup/
         cp -f /TS/updates/TorrServer /TS/TorrServer
         chmod a+x /TS/TorrServer
         /TS/TorrServer --path=/TS/db/ --port=$TS_PORT&
+    else
+        echo "Version $current_ver is latest. No update needed."
     fi
+else
+    echo "Error during the update process: no internet access or the downloaded file is corrupted."
 fi
+echo "Finished checking for updates."
+echo " "
