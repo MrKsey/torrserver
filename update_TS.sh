6d2ef7d9fdc58f94712df31cd0c0ba04891a9dc1
ksey <ksey3000@gmail.com>
2021-03-05 12:54:41 +0300

    Update update_TS.sh

--- update_TS.sh
+++ update_TS.sh
@@ -11,13 +11,24 @@ if [ $? -eq 0 -a ! -z "$updated_ver" ]; then
     current_ver=$(/TS/TorrServer --version)
     if [ "$updated_ver" != "$current_ver" ]; then
         echo "Updating to $updated_ver ..."
+        if [ ! -d "/TS/db/backup" ]; then
+            mkdir -p /TS/db/backup
+        else
+            [ -f "/TS/db/backup/TorrServer" ] && rm -f /TS/db/backup/TorrServer
+        fi 
         pkill TorrServer
-        [ -d "/TS/backup" ] && rm -r /TS/backup
-        mkdir -p /TS/backup
-        cp -f /TS/TorrServer /TS/backup/
+        cp -f /TS/TorrServer /TS/db/backup/
         cp -f /TS/updates/TorrServer /TS/TorrServer
         chmod a+x /TS/TorrServer
         /TS/TorrServer --path=/TS/db/ --port=$TS_PORT&
+        sleep 5
+        if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
+            echo "Error during the update process: downloaded file is corrupted. Restoring backup."
+            rm -f /TS/TorrServer
+            cp -f /TS/db/backup/TorrServer /TS/TorrServer
+            chmod a+x /TS/TorrServer
+            /TS/TorrServer --path=/TS/db/ --port=$TS_PORT&
+        fi
     else
         echo "Version $current_ver is latest. No update needed."
     fi
