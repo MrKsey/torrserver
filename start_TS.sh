5beebcddc35574eb330161b90f5d4194ac24a7ff
ksey <ksey3000@gmail.com>
2021-03-05 12:53:14 +0300

    Update start_TS.sh

--- start_TS.sh
+++ start_TS.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -f /TS/db/ts.ini ]; then
+if [ -f "/TS/db/ts.ini" ]; then
     . /TS/db/ts.ini && export $(grep --regexp ^[a-zA-Z] /TS/db/ts.ini | cut -d= -f1)
 fi
 
@@ -27,6 +27,29 @@ fi
 
 if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
     /TS/TorrServer --path=/TS/db/ --port=$TS_PORT&
+    sleep 5
+    if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
+        echo "Current TorrServer file is corrupted. Trying to restore backup."
+        if [ -f "/TS/db/backup/TorrServer" ]; then
+            rm -f /TS/TorrServer
+            cp -f /TS/db/backup/TorrServer /TS/TorrServer
+            chmod a+x /TS/TorrServer
+            /TS/TorrServer --path=/TS/db/ --port=$TS_PORT&
+            sleep 5
+            if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
+                echo "Fatal error!!!"
+            else
+                echo "Restore backup successful"                
+            fi
+        else
+            echo " "
+            echo "Restore backup error! Try the following:"
+            echo "1) Download the appropriate file from https://github.com/YouROK/TorrServer/releases manually, unpack and rename it to TorrServer"
+            echo "2) Put it to the directory ../db/backup/"
+            echo "3) Reboot container"
+            echo " "
+        fi
+    fi
 fi
 
 tail -f /dev/null
