2e315ced51fb5c94f9f0ed7b846dc1a7abae3f6c
ksey <ksey3000@gmail.com>
2021-10-21 02:30:18 +0300

    Update start_TS.sh

--- start_TS.sh
+++ start_TS.sh
@@ -90,10 +90,6 @@ if [ `ps | grep TorrServer | wc -w` -eq 0 ]; then
     fi
 fi
 
-if [ "$TS_RELEASE" != "latest" ]; then
-    export TS_URL=$TS_URL/tags
-fi
-
 env | grep -v cron_task | awk 'NF {sub("=","=\"",$0); print ""$0"\""}' > /TS/cron.env && chmod a+r /TS/cron.env
 
 if $TS_UPDATE ; then
