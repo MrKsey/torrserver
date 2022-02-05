38b082d06167cc55a87a9d6d5d32fa375b89ca5b
ksey <ksey3000@gmail.com>
2022-02-05 16:20:13 +0300

    Update update_TS.sh

--- update_TS.sh
+++ update_TS.sh
@@ -12,21 +12,23 @@ if [ ! -z "$BIP_URL" ]; then
     echo "=================================================="
     echo "$(date): Start checking for blacklist ip updates ..."
     
+    cd /TS/updates
+    
     EXT=$(basename $(wget -nv --spider --no-check-certificate --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0" \
     --content-disposition "$BIP_URL" -O - 2>&1 | \
     egrep -o 'http.+\w+' | cut -f 1 -d " ") | egrep -o '[^.]*$')
     
     wget -nv --no-check-certificate --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0" \
-    --content-disposition "$BIP_URL" --output-document=/TS/updates/bip_raw.$EXT
+    --content-disposition "$BIP_URL" --output-document=bip_raw.$EXT
     
     file -b --mime-type bip_raw.$EXT | ( grep -q 'text/plain' && cat bip_raw.$EXT 2>&1 || gunzip -c bip_raw.$EXT) | \
     egrep -v '^#' | tr -d "[:blank:]" | \
     egrep -o '([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | \
-    sed -r 's/0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)/\1.\2.\3.\4/g' > /TS/updates/bip.txt
+    sed -r 's/0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)/\1.\2.\3.\4/g' > bip.txt
     
-    rm -f /TS/updates/bip_raw.$EXT
+    rm -f bip_raw.$EXT
     
-    bip_size=$(wc -l /TS/updates/bip.txt | cut -f 1 -d ' ')
+    bip_size=$(wc -l bip.txt | cut -f 1 -d ' ')
     if [ $bip_size -gt 0 ]; then
         cp -f /TS/updates/bip.txt /TS/db/bip.txt
         chmod a+r /TS/db/bip.txt
